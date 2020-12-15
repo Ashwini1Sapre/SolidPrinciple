63ac44694c4d89ec5114604c604a5fdc2094f0c3
knoxpomacbookpro <himanshu.gandhi.knoxpo@gmail.com>
2020-12-15 11:42:49 +0530

    Add solid principle file to git

---
+++ SolidPrinciple/Dependency Inversion.swift
@@ -0,0 +1,58 @@
+//
+//  Dependency Inversion.swift
+//  SolidPrinciple
+//
+//  Created by Knoxpo MacBook Pro on 15/12/20.
+//
+
+import Foundation
+/*
+ A. HIGH LEVEL MODULES SHOULD NOT DEPEND UPON LOW LEVEL MODULES. BOTH SHOULD DEPEND UPON ABSTRACTIONS.
+ B. ABSTRACTIONS SHOULD NOT DEPEND UPON DETAILS. DETAILS SHOULD DEPEND UPON ABSTRACTIONS.
+*/
+
+// The Problem - Tightly coupled classes
+
+struct Wrong {
+    struct Datastore {
+        // The Datastore should not know something about the Filehandler, it should use an abstraction instead
+        let fileHandler: FileHandler = FileHandler()
+    }
+    
+    struct FileHandler {
+        func getFile(name: String) -> Data {
+            return Data()
+        }
+        
+        func storeFile(name: String, data: Data) {
+            // Stores the File
+        }
+    }
+}
+
+
+// The Solution - Protocols and DI through the initalizer
+
+protocol FileHandlerProtocol {
+    func getFile(name: String) -> Data
+    func storeFile(name: String, data: Data)
+}
+
+struct Right {
+    struct Datastore {
+        let fileHandler: FileHandlerProtocol
+        
+        init(fileHandler: FileHandlerProtocol) {
+            self.fileHandler = fileHandler
+        }
+    }
+    
+    struct FileHandler: FileHandlerProtocol {
+        func getFile(name: String) -> Data {
+            return Data()
+        }
+        func storeFile(name: String, data: Data) {
+            // Store the File
+        }
+    }
+}
---
+++ SolidPrinciple/Liskov Substitution.swift
@@ -0,0 +1,76 @@
+//
+//  Liskov Substitution.swift
+//  SolidPrinciple
+//
+//  Created by Knoxpo MacBook Pro on 15/12/20.
+//
+
+
+import Foundation
+struct Wrong {
+    class Handler {
+        
+        func save(string: String) {
+            // Save string in the Cloud
+        }
+    }
+    
+    class FilteredHandler: Handler {
+        
+        override func save(string: String) {
+            guard string.count > 5 else { return }
+            
+            super.save(string: string)
+        }
+    }
+}
+
+struct Right {
+    class Handler {
+        func save(string: String, minChars: Int = 0) {
+            guard string.count >= minChars else { return }
+        }
+    }
+}
+
+protocol Polygon {
+    var area: Float { get }
+}
+
+class Rectangle: Polygon {
+    
+    private let width: Float
+    private let length: Float
+    
+    init(width: Float, length: Float) {
+        self.width = width
+        self.length = length
+    }
+    
+    var area: Float {
+        return width * length
+    }
+}
+
+class Square: Polygon {
+    
+    private let side: Float
+    
+    init(side: Float) {
+        self.side = side
+    }
+    
+    var area: Float {
+        return pow(side, 2)
+    }
+}
+
+func printArea(of polygon: Polygon) {
+    print(polygon.area)
+}
+
+let rectangle = Rectangle(width: 2, length: 5)
+printArea(of: rectangle) // 10
+
+let square = Square(side: 2)
+printArea(of: square) // 4
